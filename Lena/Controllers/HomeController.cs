fc1aa30739f7a5b114e6bf371dec43b2fa7625e0
Furkan Sertçe <furkansertc@gmail.com>
2019-05-01 23:43:46 +0300

    LenaV1(LoginPages)

---
+++ Lena/Controllers/HomeController.cs
@@ -0,0 +1,124 @@
+﻿using Lena.App_Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Lena.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+           
+            MembershipUserCollection users = Membership.GetAllUsers();
+            return View(users);
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Register(Kullanici k)
+        {
+            MembershipCreateStatus durum;
+            Membership.CreateUser(k.KullaniciAdi, k.Parola, k.Email, k.GizliSoru, k.GizliCevap, true, out durum);
+            string mesaj="";
+            switch (durum)
+            {
+                case MembershipCreateStatus.Success:
+                    mesaj += "Kayıt Başarılı!";
+                    break;
+                case MembershipCreateStatus.InvalidUserName:
+                    mesaj += "Geçersiz Kullanıcı Adı";
+
+                    break;
+                case MembershipCreateStatus.InvalidPassword:
+                    mesaj += "Geçersiz Parola";
+                    break;
+                case MembershipCreateStatus.InvalidQuestion:
+                    mesaj += "Geçersiz Soru";
+                    break;
+                case MembershipCreateStatus.InvalidAnswer:
+                    mesaj += "Geçersiz Cevap";
+                    break;
+                case MembershipCreateStatus.InvalidEmail:
+                    mesaj += "Geçersiz Mail";
+                    break;
+                case MembershipCreateStatus.DuplicateUserName:
+                    mesaj += "Bu kullanıcı adı daha önce kullanıldı.";
+                    break;
+                case MembershipCreateStatus.DuplicateEmail:
+                    mesaj += "Bu email daha önce kullanıldı.";
+                    break;
+                case MembershipCreateStatus.UserRejected:
+                    mesaj += "Kullanıcı engel hatası";
+                    break;
+                case MembershipCreateStatus.InvalidProviderUserKey:
+                    mesaj += "Geçerssiz key hatası";
+                    break;
+                case MembershipCreateStatus.DuplicateProviderUserKey:
+                    mesaj += "Bu key daha önce kullanıldı";
+                    break;
+                case MembershipCreateStatus.ProviderError:
+                    mesaj += "Sistem hatası";
+                    break;
+                default:
+                    break;
+            }
+            ViewBag.Mesaj = mesaj;
+            if (durum==MembershipCreateStatus.Success)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            
+            return View();
+        }
+        
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login(Kullanici k, string Hatirla)
+        {
+            bool sonuc = Membership.ValidateUser(k.KullaniciAdi, k.Parola);
+            if (sonuc)
+            {
+                if (Hatirla == "on")
+                    FormsAuthentication.RedirectFromLoginPage(k.KullaniciAdi, true);
+
+                else
+                    FormsAuthentication.RedirectFromLoginPage(k.KullaniciAdi, false);
+                return RedirectToAction("Index", "Member");
+            }
+            else
+                ViewBag.Uyari = "Kullanıcı Adı veya Parola Hatalı";
+                return View();
+          
+        }
+
+        [AllowAnonymous]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+
+
+
+    }
+}
---
+++ Lena/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lena")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lena")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bcaeec66-e69f-46a4-939e-50055fb9b04d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
